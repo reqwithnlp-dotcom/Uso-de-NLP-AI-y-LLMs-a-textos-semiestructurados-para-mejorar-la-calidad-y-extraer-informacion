ed2ec08ee11ab367304efd7cfc591d01340288f9
SZ1011h <saiszr1939@gmail.com>
2026-09-03 14:33:39 -0300

    se añadio los archivos adverb y se modificaron los analicer_context

---
+++ verb_tense_inconsistencies/helpers/temporal_adverbials_helper.py
@@ -0,0 +1,51 @@
+TEMPORAL_ADBS = [
+    "since",
+    "now",
+    "by",
+    "yet",
+    "while",
+    "when",
+    "one day",
+    "for",
+]
+TEMPORAL_COMP_ADVBS = [
+    "at present",
+]
+
+FUTURE_ADBS = [
+    "tomorrow",
+    "next",
+    "soon",
+    "later",
+    "afterward",
+    "afterwards",
+    "eventually",
+    "someday",
+    "tonight",
+    "upcoming",
+    "forthcoming",
+    "following",
+    "subsequent",
+    "future",
+    "coming",
+]
+
+PAST_ADBS = [
+    "yesterday",
+    "last",
+    "ago",
+    "before",
+    "previously",
+    "earlier",
+    "formerly",
+    "recently",
+    "once",
+    "formerly",
+    "back then",
+    "in the past",
+    "former",
+    "past",
+    "preceding",
+    "prior"
+]
+
