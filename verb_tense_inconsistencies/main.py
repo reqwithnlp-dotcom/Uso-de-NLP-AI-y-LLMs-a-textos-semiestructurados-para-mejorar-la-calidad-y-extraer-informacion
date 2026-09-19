7fa727d77bb7a6889dcd9f6229af6c9506ffbd72
Julian Rabino <julianrabino@gmail.com>
2026-09-19 18:41:38 -0300

    api fix and tests

--- verb_tense_inconsistencies/main.py
+++ verb_tense_inconsistencies/main.py
@@ -1,12 +1,9 @@
 import uvicorn
 
-from api import app
-
-
 if __name__ == "__main__":
 
     uvicorn.run(
-        app,
+        "api:app",
         host="0.0.0.0",
         port=8000,
         reload=True
